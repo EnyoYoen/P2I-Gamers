fb89dd62ffd108e669ad5d665408a432bfb499c8
Cédric de Andria <cedric.de-andria@insa-lyon.fr>
2024-05-22 15:48:21 +0200

    jsp

--- Scripts/main.py
+++ Scripts/main.py
@@ -19,12 +19,7 @@ if __name__ == "__main__":
             reg_win.mainloop()
             user_id = reg_win.user_id
     if queue[0] == 'connected':
-<<<<<<< HEAD
-        print(win.user)
-        win = MainWin(win.user)
-=======
         win = MainWin(user_id)
->>>>>>> 8f98c3419b3c3a8051de840bc93a5c6cc3295f6b
         win.mainloop()
 
 
